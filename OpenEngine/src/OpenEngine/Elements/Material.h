59a3677a6cf07c00086c748d36a1f31c488f18cd
Frollo24 <57634169+Frollo24@users.noreply.github.com>
2025-07-08 17:36:16 +0200

    Moved GetProperty and SetProperty from Material.h to Material.inl

--- OpenEngine/src/OpenEngine/Elements/Material.h
+++ OpenEngine/src/OpenEngine/Elements/Material.h
@@ -55,46 +55,6 @@ namespace OpenGraphics
             return MainColor == other.MainColor;
         }
     };
-
-    template<MaterialPropertyType propType, typename T>
-    T Material::GetProperty(const std::string& name) const
-    {
-        auto propertyLocation = Properties.find(name);
-        if (propertyLocation == Properties.end())
-            return T{};
-
-        auto materialProperty = propertyLocation->second;
-        if (materialProperty.Type != propType)
-            return T{};
-
-        return *reinterpret_cast<T*>(materialProperty.Value);
-    }
-
-    template<MaterialPropertyType propType, typename T>
-    void Material::SetProperty(const std::string& name, const T& value)
-    {
-        if (auto propertyLocation = Properties.find(name); propertyLocation != Properties.end())
-        {
-            if (Properties[name].Type != propType)
-            {
-                Logger::Warn("{} property of material {} is not of type {}",
-                    name, Name, MaterialPropertyTypeToString(propType));
-                return;
-            }
-            auto oldValue = reinterpret_cast<T*>(Properties[name].Value);
-            Properties[name].Value = new T(value);
-            delete oldValue;
-        }
-        else
-        {
-            MaterialProperty newProperty{
-                .Key = name,
-                .Type = propType,
-                .Value = new T(value)
-            };
-            Properties[name] = newProperty;
-        }
-    }
 }
 
 template<> struct std::hash<OpenGraphics::Material>
@@ -104,3 +64,5 @@ template<> struct std::hash<OpenGraphics::Material>
         return hash<glm::vec4>()(material.MainColor.toGlmVector());
     }
 };
+
+#include "Material.inl"
---
+++ OpenEngine/src/OpenEngine/Elements/Material.inl
@@ -0,0 +1,44 @@
+#pragma once
+
+namespace OpenGraphics
+{
+    template<MaterialPropertyType propType, typename T>
+    T Material::GetProperty(const std::string& name) const
+    {
+        const auto propertyLocation = Properties.find(name);
+        if (propertyLocation == Properties.end())
+            return T{};
+
+        const auto materialProperty = propertyLocation->second;
+        if (materialProperty.Type != propType)
+            return T{};
+
+        return *reinterpret_cast<T*>(materialProperty.Value);
+    }
+
+    template<MaterialPropertyType propType, typename T>
+    void Material::SetProperty(const std::string& name, const T& value)
+    {
+        if (auto propertyLocation = Properties.find(name); propertyLocation != Properties.end())
+        {
+            if (Properties[name].Type != propType)
+            {
+                Logger::Warn("{} property of material {} is not of type {}",
+                    name, Name, MaterialPropertyTypeToString(propType));
+                return;
+            }
+            auto oldValue = reinterpret_cast<T*>(Properties[name].Value);
+            Properties[name].Value = new T(value);
+            delete oldValue;
+        }
+        else
+        {
+            const MaterialProperty newProperty{
+                .Key = name,
+                .Type = propType,
+                .Value = new T(value)
+            };
+            Properties[name] = newProperty;
+        }
+    }
+}
